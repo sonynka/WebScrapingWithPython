6ecfa19626466e57d2f2f8f7b4b833d71277a124
Sona Pecenakova <sona.pecenakova@gmail.com>
2017-01-18 15:04:42 +0100

    add downloads

--- htwStudiengaenge.py
+++ htwStudiengaenge.py
@@ -2,7 +2,6 @@ import requests
 import re
 from bs4 import BeautifulSoup
 import pandas as pd
-import numpy as np
 
 page = requests.get('http://www.htw-berlin.de/studium/studiengaenge/')
 
@@ -12,8 +11,8 @@ study_prog_table = soup.find('table', class_='key-value_table')
 
 progs = study_prog_table.findAll('tr')
 
-columns = [[] for x in range(7)]
-headers = ['Website', 'Name', 'Abschluss', 'Form', 'Typ', 'Art', 'Regelstudienzeit']
+columns = [[] for x in range(8)]
+headers = ['website', 'name', 'abschluss', 'form', 'typ', 'art', 'regelstudienzeit', 'ordnungen']
            
 for prog in progs:
     cells = prog.findAll(['th', 'li'])
@@ -25,9 +24,6 @@ for prog in progs:
     columns[4].append(prog.find(text=re.compile('Typ:')).split(":", 2)[1].strip())
     columns[5].append(prog.find(text=re.compile('Art:')).split(":", 2)[1].strip())
     columns[6].append(prog.find(text=re.compile('Regelstudienzeit:')).split(":", 2)[1].strip())
-
-frame = pd.DataFrame(columns).transpose()
-frame.columns = headers
 
 responses = []
 page_links = ['/studium/ordnungen-module',
@@ -45,14 +41,27 @@ for website in columns[0]:
         prog_page = requests.get(website + page_link)
         if(prog_page.status_code == 200):
             break
-    
+        
+    ordnungen = []
+        
     if(prog_page.status_code == 200):
         soup = BeautifulSoup(prog_page.content, 'html.parser')
         downloads = soup.findAll('a', class_='download')
+        for download in downloads:
+            ordnungen.append([download.get_text(), download['href']])
     else:
         print('Invalid response code for: ' + website)
-    responses.append([website, prog_page.status_code])
+        
+    responses.append([website, prog_page.status_code, downloads])
+    columns[7].append(ordnungen)
     
+
+df = pd.DataFrame(columns).transpose()
+df.columns = headers
+    
+#df.iloc[[2]]
+#df.loc[df['Name'].str.contains('Medieninformatik')]
+#df.loc[df['Website'] == 'http://bau-bachelor.htw-berlin.de']
 
 
         
